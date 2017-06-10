63cf79583fe914ca5572a5eb737d48bcf56dbd63
Ezatu丶 <qq9795439@gmail.com>
2017-06-10 15:57:12 +0800

    更新说明：新增user实例bean、userdao接口、userdaoimpl接口实现类、userdao.xml配置文件。

---
+++ src/main/java/ltd/cracks/service/front/user/UserDao.java
@@ -0,0 +1,21 @@
+package ltd.cracks.service.front.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Created by macos on 2017/6/9.
+ */
+@Transactional
+public interface UserDao {
+
+    void save(User user);
+    boolean update(User user);
+    boolean delete(int id);
+    User findById(int id);
+    List<User> findAll();
+}
